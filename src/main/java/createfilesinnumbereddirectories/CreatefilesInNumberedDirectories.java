be04708a2e5da42c10a5e2b1e232b6065ea360fe
bob <bob@streamsets.com>
2020-04-30 22:43:04 -0400

    add more featured - String datatype

--- src/main/java/createfilesinnumbereddirectories/CreatefilesInNumberedDirectories.java
+++ src/main/java/createfilesinnumbereddirectories/CreatefilesInNumberedDirectories.java
@@ -10,14 +10,31 @@ import java.util.UUID;
 
 public class CreatefilesInNumberedDirectories {
 
-  // change these as required...
+  // describe the data you'd like to create.
+  // select output format - DELIMITED or JSON
   private static final OutputTypes output = OutputTypes.JSON;
-  private static final String DIRECTORY_NAME = "/Users/bob/blif/";
-  private static final String SUB_DIR_PREFIX = "dir";
-  private static final int NUM_DIRECTORIES = 10;
+
+  // options used for JSON
+  private static final String JSON_FILENAME_SUFFIX = ".json";
+
+  // options used for DELIMITED
+  private static final String DELIMITED_DATA_FIELD_SEPARATOR = "|";
+  private static final String DELIMITED_DATA_FILENAME_SUFFIX = ".txt";
+
+  // the values will be quoted STRINGs or INTEGER.
+  private static final MyDataType dataType = MyDataType.STRING;
+
+  // specify WHERE to put the generated data:
+  // this directory will be DELETED! don't use a directory you want to keep -
+  // like a home directory or maybe Desktop.  it will be DELETED.
+  private static final String DIRECTORY_NAME = "/tmp/bobInput/";
+  private static final String SUB_DIRECTORY_PREFIX = "dir";
+
+  // specify how much generated data we should create.
+  private static final int NUM_DIRECTORIES = 5;
   private static final int FILES_PER_DIRECTORY = 5;
-  private static final int LINES_PER_FILE = 10;
-  private static final int COLUMNS_PER_LINE = 20;
+  private static final int LINES_PER_FILE = 5000;
+  private static final int COLUMNS_PER_LINE = 85;
 
   // The name says it all, but this was for the use case in Jdbc Producer where
   // the SDC Record's Fields must match the previous Record's Fields in order to
@@ -28,10 +45,6 @@ public class CreatefilesInNumberedDirectories {
   // performance for JDBC Producer.
   private static final boolean ONE_FEWER_COLUMN_ON_EVERY_OTHER_RECORD = false;
 
-  // Don't change these...
-  private static final String JSON_FILENAME_SUFFIX = ".json";
-  private static final String DELIMITED_DATA_FILENAME_SUFFIX = ".txt";
-  private static final String DELIMITED_DATA_FIELD_SEPARATOR = "|";
 
   private CreatefilesInNumberedDirectories() {
 
@@ -49,7 +62,7 @@ public class CreatefilesInNumberedDirectories {
     FileUtils.forceMkdir(new File(DIRECTORY_NAME));
 
     for (int di = 0; di < NUM_DIRECTORIES; di++) {
-      String dir = DIRECTORY_NAME + SUB_DIR_PREFIX + di;
+      String dir = DIRECTORY_NAME + SUB_DIRECTORY_PREFIX + di;
       FileUtils.forceMkdir(new File(dir));
 
       String fn;
@@ -60,7 +73,7 @@ public class CreatefilesInNumberedDirectories {
             fn = dir + File.separator + UUID.randomUUID() + JSON_FILENAME_SUFFIX;
             try (PrintWriter pw = new PrintWriter(fn)) {
               for (int line = 1; line <= LINES_PER_FILE; line++) {
-                StringJoiner join = new StringJoiner(",", "", "");
+                StringJoiner join = new StringJoiner(", ", "", "");
                 for (int col = 1; col <= COLUMNS_PER_LINE; col++) {
                   if (ONE_FEWER_COLUMN_ON_EVERY_OTHER_RECORD && col == 2 && (line % 2) == 1) {
                     continue;
@@ -69,7 +82,11 @@ public class CreatefilesInNumberedDirectories {
                     join.add(String.format("\"c%d\": %d", col, pkValue));
                     ++pkValue;
                   } else {
-                    join.add(String.format("\"c%d\": %d", col, col * line));
+                    if (dataType == MyDataType.INTEGER) {
+                      join.add(String.format("\"c%d\": %d", col, col * line));
+                    } else { // String
+                      join.add(String.format("\"c%d\": \"%s\"", col, UUID.randomUUID().toString()));
+                    }
                   }
                 }
                 pw.println("{" + join.toString() + "}");
@@ -96,7 +113,11 @@ public class CreatefilesInNumberedDirectories {
                     j2.add("" + pkValue);
                     ++pkValue;
                   } else {
-                    j2.add("" + (col * line));
+                    if(dataType == MyDataType.INTEGER) {
+                      j2.add("" + (col * line));
+                    } else {
+                      j2.add(String.format("\"%s\"", UUID.randomUUID().toString()));
+                    }
                   }
                 }
                 pw.println(j2.toString());
@@ -115,5 +136,9 @@ public class CreatefilesInNumberedDirectories {
 
   private enum OutputTypes {
     JSON, DELIMITED
+  }
+
+  private enum MyDataType {
+    INTEGER, STRING
   }
 }
